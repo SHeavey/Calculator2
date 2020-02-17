a6f8497b68c5e5027bc9ef5a66efccdb6f204de1
shaneh408 <g00325970@gmit.ie>
2020-02-17 09:57:35 +0000

    second commit test

--- src/main/java/ie/gmit/calculator.java
+++ src/main/java/ie/gmit/calculator.java
@@ -1,4 +1,28 @@
 package ie.gmit;
 
 public class calculator {
+
+    public static void main(String[] args){
+        int firstNum = 5;
+        int secondNum = 7;
+        int sum;
+    }
+    // This method will add two numbers
+    static int Addsum(int firstNum,int secondNum) {
+        return firstNum+secondNum;
+    }
+
+    static int SubtractSum(int firstNum,int secondNum) {
+        return firstNum-secondNum;
+    }
+
+    static int MultSum(int firstNum,int secondNum) {
+        return firstNum*secondNum;
+    }
+
+    static int DivSum(int firstNum,int secondNum) {
+        return firstNum*secondNum;
+    }
+
+
 }
