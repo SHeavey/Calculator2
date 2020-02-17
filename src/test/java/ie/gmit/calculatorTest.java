c8167d49fc85b6ab855af3f2ea442cf22c8f6dbe
SHANE HEAVEY - Student <G00325970@gmit.ie>
2020-02-17 09:48:15 +0000

    first test commit

---
+++ src/test/java/ie/gmit/calculatorTest.java
@@ -0,0 +1,28 @@
+package ie.gmit;
+
+public class calculatorTest {
+
+    public static void main(String[] args){
+        int firstNum = 5;
+        int secondNum = 7;
+        int sum;
+    }
+    static int Addsum(int firstNum,int secondNum) {
+        return firstNum+secondNum;
+    }
+
+    static int SubtractSum(int firstNum,int secondNum) {
+        return firstNum-secondNum;
+    }
+
+    static int MultSum(int firstNum,int secondNum) {
+        return firstNum*secondNum;
+    }
+
+    static int DivSum(int firstNum,int secondNum) {
+        return firstNum*secondNum;
+    }
+
+
+
+}
