116ddc3f1bdaa787bd346407dd2e1e0dcbcf85af
shaneh408 <g00325970@gmit.ie>
2020-02-17 10:43:15 +0000

    fourth test commit

--- src/test/java/ie/gmit/calculatorTest.java
+++ src/test/java/ie/gmit/calculatorTest.java
@@ -1,26 +1,45 @@
 package ie.gmit;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class calculatorTest {
 
-    public static void main(String[] args){
-        int firstNum = 5;
-        int secondNum = 7;
-        int sum;
-    }
-    static int Addsum(int firstNum,int secondNum) {
-        return firstNum+secondNum;
+    calculator myCalculator;
+
+    @BeforeEach
+    void init(){
+        myCalculator = new calculator();
     }
 
-    static int SubtractSum(int firstNum,int secondNum) {
-        return firstNum-secondNum;
+    @DisplayName("Addition Test Check")
+    @Test
+    void testAddNum() {
+        myCalculator = new calculator();
+        assertEquals(11,2,myCalculator.Addsum(5,6));
+    }
+    @DisplayName("Subtract Test Check")
+    @Test
+    void testSubtractSum() {
+        myCalculator = new calculator();
+        assertEquals(4,2,myCalculator.SubtractSum(10,6));
     }
 
-    static int MultSum(int firstNum,int secondNum) {
-        return firstNum*secondNum;
+    @DisplayName("Multiply Test Check")
+    @Test
+    void testMultiplyNum() {
+        myCalculator = new calculator();
+        assertEquals(20,2,myCalculator.MultSum(4,6));
     }
 
-    static int DivSum(int firstNum,int secondNum) {
-        return firstNum*secondNum;
+    @DisplayName("Divide Test Check")
+    @Test
+    void testDivideNum() {
+        myCalculator = new calculator();
+        assertEquals(3,2,myCalculator.DivSum(6,2));
     }
 
 
